fc7cf216663765430d5fd30d5fa3dee7087315c6
Lucas Fraga Mendes <lucasfragamendes@gmail.com>
2026-02-09 16:10:24 -0300

    starting project

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('Boas-vindas ao jogo do número secreto');
+let secretnumber = 29;
+let guess = prompt('Escolha um número entre 1 e 30'); 
+
+if (secretnumber == guess) {    
+    console.log('Isso ai! Você acertou');
+}
