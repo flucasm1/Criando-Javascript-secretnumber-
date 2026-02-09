cbef66b0fb43d42b384c6a749e28a3e9e489e3cb
Lucas Fraga Mendes <lucasfragamendes@gmail.com>
2026-02-09 16:31:29 -0300

    Putting reply messages

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 alert('Boas-vindas ao jogo do número secreto');
 let secretnumber = 29;
+console.log(secretnumber)
 let guess = prompt('Escolha um número entre 1 e 30'); 
 
 if (secretnumber == guess) {    
-    console.log('Isso ai! Você acertou');
+    alert(`Isso ai! Você acertou o número secreto ${secretnumber}`);
+} else {
+    alert('Você errou:(');
 }
