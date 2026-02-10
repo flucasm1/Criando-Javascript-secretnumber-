ff58d4cb680808da35cb0113e8fd0ce036661e1e
Lucas Fraga Mendes <lucasfragamendes@gmail.com>
2026-02-09 21:59:20 -0300

    Add repetition with "while"

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 alert('Boas-vindas ao jogo do número secreto');
 let secretnumber = 2;
 console.log(secretnumber)
-let guess = prompt('Escolha um número entre 1 e 30'); 
-    
-if (secretnumber == guess) {    
+let guess 
+
+while (guess != secretnumber) {
+    guess = prompt('Escolha um número entre 1 e 30'); 
+    if (secretnumber == guess) {    
     alert(`Isso ai! Você acertou o número secreto ${secretnumber}`);
 }  else { 
     if  (secretnumber > guess) {
@@ -12,3 +14,6 @@ if (secretnumber == guess) {
     alert('O número secreto é menor que ' + guess);
     }
 }
+}
+
+
