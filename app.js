172a9c17f6a1cf3255d87d84aed24b3171f786cc
Lucas Fraga Mendes <lucasfragamendes@gmail.com>
2026-02-09 22:07:04 -0300

    Add number of tries

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 alert('Boas-vindas ao jogo do número secreto');
-let secretnumber = 2;
-console.log(secretnumber)
-let guess 
+let secretnumber = 10;
+console.log(secretnumber);
+let guess;
+let tries = 1;
 
 while (guess != secretnumber) {
     guess = prompt('Escolha um número entre 1 e 30'); 
     if (secretnumber == guess) {    
     alert(`Isso ai! Você acertou o número secreto ${secretnumber}`);
+    alert('Número de tentativas = ' + tries)
 }  else { 
+    tries += 1 //ou tries ++
     if  (secretnumber > guess) {
         alert ('O número secreto é maior que ' + guess);
     }   else {
@@ -15,5 +18,6 @@ while (guess != secretnumber) {
     }
 }
 }
+ 
 
 
