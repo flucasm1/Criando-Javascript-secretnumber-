594e4c51cac5a2b11870aaecedb70f7667ab26c7
Lucas Fraga Mendes <lucasfragamendes@gmail.com>
2026-02-09 21:52:40 -0300

    Add comparative messages (higher or lower)

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ alert('Boas-vindas ao jogo do número secreto');
 let secretnumber = 2;
 console.log(secretnumber)
 let guess = prompt('Escolha um número entre 1 e 30'); 
-
+    
 if (secretnumber == guess) {    
     alert(`Isso ai! Você acertou o número secreto ${secretnumber}`);
-} else {
-    alert('Você errou:(');
+}  else { 
+    if  (secretnumber > guess) {
+        alert ('O número secreto é maior que ' + guess);
+    }   else {
+    alert('O número secreto é menor que ' + guess);
+    }
 }
